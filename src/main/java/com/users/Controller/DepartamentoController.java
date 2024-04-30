d9af9660b4049b325a90422b748707509a63fe5f
CamiloDev404 <crosti1826@gmail.com>
2024-04-29 21:47:49 -0500

    Documentacion del microservicio

--- src/main/java/com/users/Controller/DepartamentoController.java
+++ src/main/java/com/users/Controller/DepartamentoController.java
@@ -2,6 +2,7 @@ package com.users.Controller;
 
 import com.users.Model.Departamento;
 import com.users.Service.DepartamentoService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/departamento")
 @CrossOrigin("*")
+@Tag(name = "Departamentos")
 public class DepartamentoController {
 
     @Autowired
